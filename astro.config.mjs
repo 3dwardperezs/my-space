fd52a6a93fcb361c56e09b742ed98ba548f259c1
Certa2022 <98969586+Certa2022@users.noreply.github.com>
2024-08-02 22:21:55 +0100

    firts commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  site:'https://myspace.github.io',
   output: "server",
-  base: '/my-repo/'
+  
 });
